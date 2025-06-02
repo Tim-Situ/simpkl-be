979ec40224db90cc5115bf7bb14a3ea9c86fba0a
fauzein <fauzeinmw2@gmail.com>
2025-06-02 15:51:41 +0700

    Test Pipeline

--- test/pengumuman.test.js
+++ test/pengumuman.test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 describe('API Pengumuman', () => {
     it('GET /pengumuman - should return all items', async () => {
         const res = await request(app).get('/pengumuman/all');
+        
         expect(res.statusCode).toBe(200);
     });
 })
