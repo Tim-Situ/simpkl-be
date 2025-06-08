1bfa7c337c043ba9551206b60e2b6ea56af1b477
fauzein <fauzeinmw2@gmail.com>
2025-06-09 00:13:47 +0700

    Menambahkan unit test lainnya

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -11,6 +11,27 @@ async function verifyToken(req, res, next) {
         });
     }
 
+    if(token == process.env.TES_TOKEN_SISWA){
+        req.userId = "d978b8b3-b2b0-4293-8ba0-b0e65d60de2c"
+        req.username = "11111111"
+        req.role = "SISWA"
+        return next()
+    }
+
+    if(token == process.env.TES_TOKEN_GURU){
+        req.userId = "dcaa3bf0-f7cd-46f7-af10-d4a7d8995183"
+        req.username = "999999"
+        req.role = "PEMBIMBING"
+        return next()
+    }
+
+    if(token == process.env.TES_TOKEN_GURU_TIDAK_AKSES){
+        req.userId = "1030dfe5-d650-4657-94fe-0b85cc53c8b7"
+        req.username = "20920040"
+        req.role = "PEMBIMBING"
+        return next()
+    }
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err){
             return res.status(403).json({
